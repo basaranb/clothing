25de1705bed8222aa847f0d7f09c96440eb7e13f
Basaran Badur <basaranbadur@gmail.com>
2020-06-15 10:35:10 +0300

    conveted directory state to redux, added selector

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
 import { cartReducer } from "./cart/cart.reducer";
+import { directoryReducer } from "./directory/directory.reducer";
 
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // local storage
@@ -15,6 +16,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
